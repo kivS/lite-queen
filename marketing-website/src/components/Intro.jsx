1848ac3e90188df0b645e6f3d813339558d51189
Vik Borges <v1k@protonmail.com>
2024-09-25 17:51:40 +0800

    🚀

--- marketing-website/src/components/Intro.jsx
+++ marketing-website/src/components/Intro.jsx
@@ -22,14 +22,24 @@ export function Intro() {
 				databases on your server with{" "}
 				<span className="text-landing-accent-light">ease</span>
 			</h1>
-			<p className="mt-8 text-base/6 dark:text-gray-300 text-pretty text-center">
+
+			<p className="mt-8 font-medium text-lg/6 dark:text-gray-300 text-pretty text-center">
 				Lite Queen is an open-source SQLite database management software that
 				runs on your server.
+				<a
+					target="_blank"
+					rel="noreferrer"
+					href="https://github.com/kivS/lite-queen"
+					className="flex mt-4 items-center gap-1 border p-1 justify-center w-2/4 mx-auto hover:scale-105 hover:text-landing-accent-light  dark:border-white  rounded-lg font-medium "
+				>
+					<GitHubIcon className="size-4" />
+					<span className="text-sm">kivS/lite-queen</span>
+				</a>
 			</p>
 
 			{/* <SignUpForm /> */}
 
-			<div className="mt-10 flex flex-col flex-wrap justify-center gap-8  lg:justify-start">
+			<div className="mt-4 flex flex-col flex-wrap justify-center gap-8  lg:justify-start">
 				{/* <IconLink href="#" icon={BookIcon} className="flex-none">
           Documentation
         </IconLink> */}
@@ -37,10 +47,10 @@ export function Intro() {
           DEMO
         </IconLink> */}
 
-				<DownloadDropdown />
-
-				<div className="mt-8">
+				<div className="my-12 flex flex-col gap-10">
 					<DemoButton />
+
+					<DownloadDropdown />
 				</div>
 			</div>
 		</>
@@ -49,7 +59,7 @@ export function Intro() {
 
 function DemoButton() {
 	return (
-		<div className="flex flex-row  w-full gap-1 justify-center mb-4 dark:text-white ">
+		<div className="flex flex-row  gap-1 justify-center  dark:text-white ">
 			{/* <FeaturesCarousel />
 
 					<IconSlash className="dark:text-white self-center size-4" /> */}
@@ -160,7 +170,7 @@ function BookIcon(props) {
 	);
 }
 
-function GitHubIcon(props) {
+export function GitHubIcon(props) {
 	return (
 		<svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
 			<path d="M8 .198a8 8 0 0 0-8 8 7.999 7.999 0 0 0 5.47 7.59c.4.076.547-.172.547-.384 0-.19-.007-.694-.01-1.36-2.226.482-2.695-1.074-2.695-1.074-.364-.923-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.224 1.873.87 2.33.666.072-.518.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.954 0-.873.31-1.586.823-2.146-.09-.202-.36-1.016.07-2.118 0 0 .67-.214 2.2.82a7.67 7.67 0 0 1 2-.27 7.67 7.67 0 0 1 2 .27c1.52-1.034 2.19-.82 2.19-.82.43 1.102.16 1.916.08 2.118.51.56.82 1.273.82 2.146 0 3.074-1.87 3.75-3.65 3.947.28.24.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.14.46.55.38A7.972 7.972 0 0 0 16 8.199a8 8 0 0 0-8-8Z" />
