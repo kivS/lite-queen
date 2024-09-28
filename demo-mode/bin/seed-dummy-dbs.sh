5cbecccae53e5295ce49b30a7411e41a90cc82d8
Vik Borges <v1k@protonmail.com>
2024-09-28 14:05:27 +0800

    🚀

---
+++ demo-mode/bin/seed-dummy-dbs.sh
@@ -0,0 +1,19 @@
+echo "Seeding the dummy databases"
+
+declare -a dbs=(
+    # file name and file path inside of container
+    "simplefolks.sqlite /app/databases/simplefolks.sqlite"
+    "travelbuddy.db /app/databases/travelbuddy.db"
+    # "census2000names /app/databases/census2000names.sqlite"
+    # Add more databases here
+)
+for db_entry in "${dbs[@]}"; do
+    IFS=' ' read -r db_alias db_path <<< "$db_entry"
+    ssh $myvps_host -p 54321 "curl --location --request POST 'http://localhost:8000/api/load-database' \
+    --header 'Cache-Control: no-cache' \
+    --header 'Accept: */*' \
+    --header 'Accept-Encoding: gzip, deflate' \
+    --header 'Connection: keep-alive' \
+    --data-urlencode 'db_path=$db_path' \
+    --data-urlencode 'db_alias=$db_alias'"
+done
