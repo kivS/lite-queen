f6fb69d02baf4821fc73a4d787058273d94d0511
Vik Borges <v1k@protonmail.com>
2024-10-13 15:44:33 +0800

    🚀

--- marketing-website/src/components/Intro.jsx
+++ marketing-website/src/components/Intro.jsx
@@ -7,7 +7,7 @@ import { Button } from "./Button";
 import Script from "next/script";
 import BuyCard from "./buy-card";
 import FeaturesCarousel from "./features-carousel";
-import { DrumstickIcon, IconSlash, X } from "./icons";
+import { BookIcon, DrumstickIcon, IconSlash, X } from "./icons";
 
 import { DialogContent } from "@radix-ui/react-dialog";
 import { Dialog } from "./ui/dialog";
@@ -105,7 +105,7 @@ function DemoButton() {
 export function IntroFooter() {
 	return (
 		<p className="flex items-baseline  gap-x-2 text-[0.8125rem]/6 text-gray-500">
-			<IconLink href="/guides" icon={FaqIcon} className="flex-none">
+			<IconLink href="/guides" icon={BookIcon} className="flex-none">
 				Guides
 			</IconLink>
 			<span>/</span>
@@ -192,13 +192,13 @@ function FaqIcon(props) {
 	);
 }
 
-function BookIcon(props) {
-	return (
-		<svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
-			<path d="M7 3.41a1 1 0 0 0-.668-.943L2.275 1.039a.987.987 0 0 0-.877.166c-.25.192-.398.493-.398.812V12.2c0 .454.296.853.725.977l3.948 1.365A1 1 0 0 0 7 13.596V3.41ZM9 13.596a1 1 0 0 0 1.327.946l3.948-1.365c.429-.124.725-.523.725-.977V2.017c0-.32-.147-.62-.398-.812a.987.987 0 0 0-.877-.166L9.668 2.467A1 1 0 0 0 9 3.41v10.186Z" />
-		</svg>
-	);
-}
+// function BookIcon(props) {
+// 	return (
+// 		<svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+// 			<path d="M7 3.41a1 1 0 0 0-.668-.943L2.275 1.039a.987.987 0 0 0-.877.166c-.25.192-.398.493-.398.812V12.2c0 .454.296.853.725.977l3.948 1.365A1 1 0 0 0 7 13.596V3.41ZM9 13.596a1 1 0 0 0 1.327.946l3.948-1.365c.429-.124.725-.523.725-.977V2.017c0-.32-.147-.62-.398-.812a.987.987 0 0 0-.877-.166L9.668 2.467A1 1 0 0 0 9 3.41v10.186Z" />
+// 		</svg>
+// 	);
+// }
 
 export function GitHubIcon(props) {
 	return (
