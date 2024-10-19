e7904baa4b2280f295f9cd765e2c5f402e9f3b1c
Vik Borges <v1k@protonmail.com>
2024-10-19 10:59:41 +0800

    🚀

---
+++ bin/build_binaries_for_distribution.sh
@@ -0,0 +1,42 @@
+echo "Running script: $(basename "$0")"
+
+
+DATE=$(date +%Y-%m-%d)
+
+
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
+    echo " ❌ Not on the main branch. Skipping..."
+    exit 0
+fi
+
+cd _ui/
+
+
+if ! npm run build; then
+    echo " ❌ Failed to build. Stopping the script."
+    exit 1
+fi
+
+
+if ! npm run generate-static-bundle; then
+    echo " ❌ Failed to generate static bundle. Stopping the script."
+    exit 1
+fi
+
+
+cd ../executable
+
+
+cp ../_ui/static-ui-bundle.js static-ui-bundle.js
+
+
+bun run build-local
+bun run build-dist-linux-x64
+bun run build-dist-linux-arm64
+bun run build-dist-darwin-x64
+bun run build-dist-darwin-arm64
+
+
+
+
